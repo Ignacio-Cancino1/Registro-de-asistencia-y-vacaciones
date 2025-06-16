0b708607860328c60f14d0ef0e04920b4a9a8ffd
Ignacio Cancino <icancino2021@alu.uct.cl>
2025-06-15 23:26:44 -0400

    Update api.js

--- Registro_Asis/src/services/api.js
+++ Registro_Asis/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: import.meta.env.VITE_API_URL,
 });
+
 
 
 // Agregar token JWT automáticamente
